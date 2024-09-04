85e9fe8cbbb981a88e573d8bdfd43b8e8fae7db1
jw <josephwhyman@gmail.com>
2024-09-04 12:05:05 +0100

    corrected a comment and commented out some non functioning code

--- bot.py
+++ bot.py
@@ -57,17 +57,16 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        embed=discord.Embed(title="Hello World!", description=" MHIS is a bot that (for now) acts as the interface between Discord ,meshtastic (and hopefully soon the physical maidstone hackspace)", color=0x00ff00)
-        embed.set_author(name="Maidstone Hackspace Info System", icon_url="https://maidstone-hackspace.org.uk/wp-content/uploads/2022/09/cropped-hackspace_960x540.png")
-        await message.channel.send(embed=embed)
-        print('hello world')
+    #if message.content.startswith('$hello'):
+     #   embed=discord.Embed(title="Hello World!", description=" MHIS is a bot that (for now) acts as the interface between Discord ,meshtastic (and hopefully soon the physical maidstone hackspace)", color=0x00ff00)
+      #  embed.set_author(name="Maidstone Hackspace Info System", icon_url="https://maidstone-hackspace.org.uk/wp-content/uploads/2022/09/cropped-hackspace_960x540.png")
+       # await message.channel.send(embed=embed)
+        #print('hello world')
         
         
-    #if message.content.startswith('$ping'):
-      #  await message.channel.send('Pong')
-      #  await edit(nick='Macaroni Dispenser')
-      #  print('pong')
+    if message.content.startswith('$ping'):
+        await message.channel.send('Pong')
+        print('pong')
 
 
 client.run(TOKEN)
